d92c6c6601c5148afd91399c9a41cb2459ffbdf0
YukaiZhang <yzhanghe@connect.ust.hk>
2025-12-08 14:01:19 +0800

    handle single channel depth image

--- datasets/transforms/formatting.py
+++ datasets/transforms/formatting.py
@@ -19,8 +19,14 @@ class ToTensor():
                 sample[key] = self._array_to_tensor(sample[key])
             elif key.startswith('input_'):
                 sample[key] = self._list_to_tensor(sample[key])
-                if key.startswith('input_rgb') or key.startswith('input_depth'):
+                if key.startswith('input_rgb'):
                     sample[key] = sample[key].permute(0, 3, 1, 2)  # T H W C -> T C H W
+                elif key.startswith('input_depth'):
+                    # Handle depth: if grayscale (T, H, W), expand to (T, 3, H, W)
+                    if sample[key].ndim == 3:  # (T, H, W)
+                        sample[key] = sample[key].unsqueeze(1).repeat(1, 3, 1, 1)  # -> (T, 3, H, W)
+                    else:  # (T, H, W, C)
+                        sample[key] = sample[key].permute(0, 3, 1, 2)  # T H W C -> T C H W
                     
             elif key in ['idx']:
                 sample[key] = self._item_to_tensor(sample[key], dtype=torch.long)
