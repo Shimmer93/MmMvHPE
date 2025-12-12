758ccf9b7ced01a9423613edca827bfc57f757ca
YukaiZhang <yzhanghe@connect.ust.hk>
2025-12-12 14:58:40 +0800

    import RegressionCameraHead

--- models/heads/__init__.py
+++ models/heads/__init__.py
@@ -1,6 +1,6 @@
 from .xfi_head import XFiRegressionHead  # noqa: F401, F403
 from .vggt_camera_head import VGGTCameraHead  # noqa: F401, F403
-from .regression_head import RegressionKeypointHead  # noqa: F401, F403
+from .regression_head import RegressionKeypointHead, RegressionCameraHead  # noqa: F401, F403
 
 __all__ = [
     'XFiRegressionHead',
