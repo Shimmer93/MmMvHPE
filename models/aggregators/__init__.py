75bbae23ca3a6200fd688d3c26b9b6ff339199e7
Shimmer93 <zpengac@connect.ust.hk>
2025-12-20 00:47:49 +0800

    fix model init

--- models/aggregators/__init__.py
+++ models/aggregators/__init__.py
@@ -2,12 +2,10 @@ from .xfi_aggregator import XFiAggregator  # noqa: F401, F403
 from .trans_aggregator import TransformerAggregator  # noqa: F401, F403
 from .trans_aggregator_v2 import TransformerAggregatorV2  # noqa: F401, F403
 from .trans_aggregator_v3 import TransformerAggregatorV3  # noqa: F401, F403
-from .trans_aggregator_v5 import TransformerAggregatorV5  # noqa: F401, F403
 
 __all__ = [
     'XFiAggregator',
     'TransformerAggregator',
     'TransformerAggregatorV2',
     'TransformerAggregatorV3',
-    'TransformerAggregatorV5',
 ]
