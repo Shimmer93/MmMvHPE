cb74b9ce5de8d1f0145718bd033196eed650c3b8
Shimmer93 <zpengac@connect.ust.hk>
2026-01-09 21:26:43 +0800

    ablation

--- models/heads/regression_head_v2.py
+++ models/heads/regression_head_v2.py
@@ -5,7 +5,7 @@ from einops import rearrange, repeat
 from .base_head import BaseHead
 
 class RegressionKeypointHeadV2(BaseHead):
-    def __init__(self, losses, emb_size=512):
+    def __init__(self, losses, emb_size=512, only_last_layer=False):
         super().__init__(losses)
         self.emb_size = emb_size
         
@@ -16,10 +16,15 @@ class RegressionKeypointHeadV2(BaseHead):
             nn.Linear(emb_size//2, 3)
         )
 
+        self.only_last_layer = only_last_layer
+
     def forward(self, x):
         if isinstance(x, list):
             # print(len(x))
-            x = torch.concatenate(x, dim=-1)
+            if self.only_last_layer:
+                x = x[-1]
+            else:
+                x = torch.concatenate(x, dim=-1)
         # print(x.shape)
         # x.shape: B, M, T, J+1, C
         B, M, T, N, C = x.shape
